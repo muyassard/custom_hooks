1ec0bdbc3b05e1eb496ae62984c7aae76a3f77d3
muyassard <muyassarsolijonova@gmail.com>
2024-02-08 19:43:18 +0500

    ci: added uselocalstorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,27 +1,41 @@
-import { useLocalStorage } from "@uidotdev/usehooks";
-import React from "react";
+// import { useLocalStorage } from "@uidotdev/usehooks";
+import React, { useEffect, useState } from "react";
+import store from "store2";
 
-// Usage
-export const LocalStorage:React.FC = () => {
+import { Button } from "antd";
+
+const useLocalStorage = (key: string, initialValue: any) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = store.get(key);
+    return storedValue !== undefined ? storedValue : initialValue;
+  });
+
+  useEffect(() => {
+    store.set(key, value);
+  }, [key, value]);
+  return [value, setValue];
+};
+
+export const LocalStorage: React.FC = () => {
   const [value, setValue] = useLocalStorage("test-key", 0);
 
   return (
     <div>
       <p>Count: {value}</p>
-      <button
+      <Button
         onClick={() => {
           setValue((x: number) => x + 1);
         }}
       >
         Increment
-      </button>
-      <button
+      </Button>
+      <Button
         onClick={() => {
           setValue((x: number) => x - 1);
         }}
       >
         Decrement
-      </button>
+      </Button>
     </div>
   );
 };
