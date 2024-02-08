7d02dce386fcec013ea91319af4a80de2f5a14b9
muyassard <muyassarsolijonova@gmail.com>
2024-02-08 17:16:31 +0500

    usekeypress is not written as huk

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -4,49 +4,60 @@ import React, { useEffect } from "react";
 const useKeyPress = () => {};
 
 export const KeyPress = () => {
+  const [activeKey, setActiveKey] = React.useState("");
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       const pressedKey = event.key.toLowerCase();
+
       if (/[a-z]/.test(pressedKey)) {
-        console.log(`The letter "${pressedKey}" is pressed`);
+        setActiveKey(pressedKey);
+        // console.log( pressedKey);
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
   }, []);
-  const letters = [
-    "q",
-    "w",
-    "e",
-    "r",
-    "t",
-    "y",
-    "u",
-    "i",
-    "o",
-    "p",
-    "a",
-    "s",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "z",
-    "x",
-    "c",
-    "v",
-    "b",
-    "n",
-    "m",
-  ];
+  const letters1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+  const letters2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+  const letters3 = ["z", "x", "c", "v", "b", "n", "m"];
+  const classname = `p-5 rounded-lg bg-blue-200 `;
   return (
-    <div className="h-[100%]  grid place-items-center">
-      <Button className="">key</Button>
-      <div className="">
-        
+    <div className="h-[100%]  grid place-items-center mt-10">
+      <div className="flex flex-col gap-3 items-center">
+        <div className="flex gap-2">
+          {letters1.map((letter, index) => (
+            <div
+              className={
+                classname + `${letter === activeKey ? "bg-blue-400" : ""}`
+              }
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          {letters2.map((letter, index) => (
+            <div
+              className={
+                classname + `${letter === activeKey ? "bg-blue-400" : ""}`
+              }
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          {letters3.map((letter, index) => (
+            <div
+              className={
+                classname + `${letter === activeKey ? "bg-blue-400" : ""}`
+              }
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
